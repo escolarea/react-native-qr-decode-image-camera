c53e706ede25b8801f847ccc4f2e928728f99d10
Alex-Icoms <alex@icoms.co>
2022-05-23 15:26:00 -0600

    aling bracket

--- android/src/main/java/com/lewin/qrcode/QRScanReader.java
+++ android/src/main/java/com/lewin/qrcode/QRScanReader.java
@@ -61,7 +61,7 @@ public class QRScanReader extends ReactContextBaseJavaModule  {
                                 scanner.close();
                                 promise.reject("NOT_OK", "No related QR code");
                                 return;
-                          }
+                            }
 
                             Log.d("OK", " " +  barcodes.toString());
                             List<String> rawValues = new LinkedList<>();
